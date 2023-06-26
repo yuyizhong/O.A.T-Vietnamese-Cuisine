4e41f26b962b5a5b7d4a8f40bdbb24f228b122f1
yuyizhong <>
2023-06-26 11:04:09 +0000

    import paginator and creat review_list

--- review/views.py
+++ review/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def review_list(request):
+    reviews = Review.objects.all()
+
+    # Set the number of items per page
+    items_per_page = 10
+
+    paginator = Paginator(reviews, items_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'review_list.html', context)

69808042e2b63cb67ac4b0eb7a47a711099e4bb4
yuyizhong <>
2023-06-26 14:43:43 +0000

    Add index.html and statics

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    """
+    View to render homepage
+    """
+    template_name = 'home/index.html'

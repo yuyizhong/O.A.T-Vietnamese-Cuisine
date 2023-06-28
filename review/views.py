4e9c71718bd467349245c7107f6f3ec25114e00d
Yuyi Zhong <zoe_yyz@hotmail.com>
2023-06-28 00:56:53 +0000

    Reinstalled requirements.txt and fix rating error

--- review/views.py
+++ review/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from .models import Review
 
 
 def review_list(request):
     reviews = Review.objects.all()
 
-    # Set the number of items per page
-    items_per_page = 10
+    # # Set the number of items per page
+    # items_per_page = 10
 
-    paginator = Paginator(reviews, items_per_page)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    # paginator = Paginator(reviews, items_per_page)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
 
-    context = {
-        'page_obj': page_obj,
+   
+    
+    paginate_by = 6
+     context = {
+        'reviews': review,
+        
+        # 'page_obj': page_obj,
     }
 
-    return render(request, 'review_list.html', context)
+
+    return render(request, 'review/reviews.html', context)

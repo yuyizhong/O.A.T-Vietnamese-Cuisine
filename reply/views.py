e9980dded7b9a5bfdf13dba0ba8b742f0a2da0e8
yuyizhong <zoe_yyz@hotmail.com>
2023-07-04 21:19:47 +0000

    Add reply view

--- reply/views.py
+++ reply/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Reply
+from review.models import Review
+from .forms import ReplyForm
 
-# Create your views here.
+def leave_review(request, pk):
+
+    review = Review.objects.get(id=pk)
+    form = ReplyForm()
+
+    if request.method == 'POST':
+        form = ReplyForm(request.POST, request.FILES)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+          
+            reply.review = review
+            reply.save()
+        return redirect('item-reviews') 
+
+    context = {
+        'form': form,
+        'menu_id': menu.id,
+        'review': review
+    }
+
+    return render(request, 'reply/reply.html', context)

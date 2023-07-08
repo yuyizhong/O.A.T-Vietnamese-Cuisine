9432b0d2d387fa9b38851251987a5494f0538ec9
yuyizhong <zoe_yyz@hotmail.com>
2023-07-08 23:45:19 +0000

    Amend css and js files for index page for best outcome

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,23 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var containerSections = document.querySelectorAll(".container-section");
-    var currentIndex = 0;
+  var containerSections = document.querySelectorAll(".container-section");
+  var delay = 0;
 
-    // Show the first container-section initially
-    containerSections[currentIndex].style.display = "flex";
+  containerSections.forEach(function(section) {
+    setTimeout(function() {
+      section.style.opacity = "1";
+      section.style.transform = "translateY(0)";
+    }, delay);
 
-    setInterval(function() {
-        // Hide the current container-section
-        containerSections[currentIndex].style.display = "none";
-
-        // Increment the currentIndex
-        currentIndex++;
-
-        // Reset the index to 0 if it exceeds the number of container-sections
-        if (currentIndex >= containerSections.length) {
-            currentIndex = 0;
-        }
-
-        // Show the next container-section
-        containerSections[currentIndex].style.display = "flex";
-    }, 5000);
+    delay += 200;
+  });
 });

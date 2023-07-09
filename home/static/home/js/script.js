ba706af0edd62c7de7c930834d9a834a694d6b68
yuyizhong <zoe_yyz@hotmail.com>
2023-07-09 16:23:23 +0000

    Amend the css, js files for home page

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var containerSections = document.querySelectorAll(".container-section");
-  var delay = 0;
+  const containerSections = document.querySelectorAll(".container-section");
+  let currentSectionIndex = 0;
 
-  containerSections.forEach(function(section) {
-    setTimeout(function() {
-      section.style.opacity = "1";
-      section.style.transform = "translateY(0)";
-    }, delay);
+  function showNextSection() {
+      containerSections[currentSectionIndex].classList.remove("active");
+      currentSectionIndex = (currentSectionIndex + 1) % containerSections.length;
+      containerSections[currentSectionIndex].classList.add("active");
+  }
 
-    delay += 200;
-  });
+  // Show the first section initially
+  containerSections[currentSectionIndex].classList.add("active");
+
+  // Start the interval to show the next section every 3 seconds
+  setInterval(showNextSection, 3000);
 });

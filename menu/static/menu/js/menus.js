0154571090980ed65d30d020c2446bdd267ca2ef
yuyizhong <zoe_yyz@hotmail.com>
2023-07-11 01:09:08 +0000

    Create modal for menu-list page and add js and css code accordingly

---
+++ menu/static/menu/js/menus.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuModal = document.getElementById('menuModal');
+    var menuModalTitle = document.getElementById('menuModalTitle');
+    var menuDescription = document.getElementById('menuDescription');
+
+    menuModal.addEventListener('show.bs.modal', function(event) {
+        var button = event.relatedTarget;
+        var menuNameValue = button.getAttribute('data-menu-name');
+        var menuDescriptionValue = button.getAttribute('data-menu-description');
+
+        menuModalTitle.textContent = menuNameValue;
+        menuDescription.textContent = menuDescriptionValue;
+    });
+});

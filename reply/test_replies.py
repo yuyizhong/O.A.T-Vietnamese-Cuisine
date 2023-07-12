495554e170497d54f7449e71411c7bb3d7c9b3d9
yuyizhong <zoe_yyz@hotmail.com>
2023-07-12 21:10:54 +0000

    Start reply view test initial set up

--- reply/test_replies.py
+++ reply/test_replies.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Reply
+from review.models import Review
 
-# Create your tests here.
+class ReplyViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.review = Review.objects.create(title='Test Review')
+        self.url = reverse('reply/<int:pk>/', kwargs={'pk': self.review.pk})
+        
+    

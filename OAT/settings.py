690db9fb8ba0bf5cec3166517edb53fa73cca384
yuyizhong <zoe_yyz@hotmail.com>
2023-07-18 23:30:30 +0000

    Amend js error and change the DEBUG value

--- OAT/settings.py
+++ OAT/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEVELOPMENT']
 
 ALLOWED_HOSTS = [
     '8000-yuyizhong-oatvietnamese-94a23rskak1.ws-eu101.gitpod.io',

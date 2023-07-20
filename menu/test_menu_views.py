f7b0ca10906a2addeaccce968aeb33c571bafd7f
yuyizhong <zoe_yyz@hotmail.com>
2023-07-20 21:48:44 +0000

    Amend dentication error in doc string

--- menu/test_menu_views.py
+++ menu/test_menu_views.py
@@ -26,15 +26,19 @@ class MenuAppTests(TestCase):
         )
 
     def test_menu_list_view(self):
-        """ Test menu-list accessability, template render to any users"""
+        """ 
+        Test menu-list accessability, template render to any users
+        """
         url = reverse('menu-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'menu/menu_list.html')
 
     def test_add_menu_view(self):
-        """ Test add_menu_view access to staff user only 
-        with correct template render """
+        """ 
+        Test add_menu_view access to staff user only 
+        with correct template render 
+        """
         self.client.force_login(self.user)
         url = reverse('add-menu')
         response = self.client.get(url)
@@ -42,8 +46,11 @@ class MenuAppTests(TestCase):
         self.assertTemplateUsed(response, 'menu/add_menu.html')
 
     def test_edit_menu_view(self):
-         """ Test edit_menu_view access to staff user only 
-        with correct template render """
+
+        """ 
+        Test edit_menu_view access to staff user only 
+        with correct template render 
+        """
         self.client.force_login(self.user)
         url = reverse('edit-menu', args=[self.menu_item.id])
         response = self.client.get(url)
@@ -51,7 +58,9 @@ class MenuAppTests(TestCase):
         self.assertTemplateUsed(response, 'menu/edit_menu.html')
 
     def test_hide_menu_view(self):
-         """ Test hind/unhide_menu function to staff user only """           
+        """ 
+        Test hind/unhide_menu function to staff user only 
+        """           
         self.client.force_login(self.user)
         url = reverse('hide-menu', args=[self.menu_item.id])
         response = self.client.get(url)
@@ -59,7 +68,9 @@ class MenuAppTests(TestCase):
         self.assertEqual(response.status_code, 302)  
 
     def test_delete_menu_view(self):
-        """ Test delete_menu function to staff user only """   
+        """ 
+        Test delete_menu function to staff user only 
+        """   
         self.client.force_login(self.user)
         url = reverse('delete-menu', args=[self.menu_item.id])
         response = self.client.get(url)
